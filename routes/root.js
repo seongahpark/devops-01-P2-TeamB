d7d27da23ea0bcb056c99c2836111104aa609141
seongahpark <sksmsekekek21@naver.com>
2022-03-31 16:19:52 +0900

    [chore] routes/root.js edit

--- routes/root.js
+++ routes/root.js
@@ -2,6 +2,8 @@
 
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
-    return { root: true }
+    reply
+      .code(200)
+      .send("hello world")
   })
 }
